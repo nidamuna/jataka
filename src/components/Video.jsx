8d750009b6e847b132b423a139f045583b93cd74
nidamuna <munanida@gmail.com>
2022-10-01 01:14:16 +0700

    fixed video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import Borobudur2 from '../assets/borobudur.webm';
 
-const Video = () => {
+const Video2 = () => {
   return (
-    <div className="w-[100%] h-full mx-auto">
-      <div className="relative w-[100%] mx-auto">
-        <video loop autoPlay muted className="w-[100%] h-full">
-          <source src={Borobudur2} type="video/webm" />
+    <div
+        className="relative flex items-center justify-center h-screen overflow-hidden w-screen">
+        <div className="relative px-[96px] pt-[150px] z-30 bg-black/50 w-full h-screen flex flex-col justify-center">
+            <h1 className="font-bold leading-[77px] text-white text-[64px] mb-[22px]">
+                Welcome to Jataka
+            </h1>
+            <p className="text-white text-[20px] font-mendium leading-[24px] mb-[22px]">
+                 World first Metaverse for Heritage cultural object preservation
+            </p>
+            <button 
+                className="flex mt-[22px] justify-center py-[12px] px-[59px] text-white font-medium text-[16px] leading-[19px] rounded-lg bg-[#D21919] gap-[10px] w-[160px]">
+                Explore
+            </button>
+        </div>
+        <video
+            autoPlay
+            loop
+            muted
+            className="absolute z-10 w-auto min-w-full min-h-full max-w-none">
+            <source src={Borobudur2} type="video/webm"/>
+            Your browser does not support the video tag.
         </video>
-        <div className="absolute w-[100%] inset-0 h-full bg-black/60 flex flex-col items-start justify-center px-[96px]">
-          <h1 className="font-bold leading-[77px] text-white text-[64px] mb-[22px]">
-            Welcome to Jataka
-          </h1>
-          <p className="text-white text-[20px] font-mendium leading-[24px] mb-[22px]">
-            World first Metaverse for Heritage cultural object preservation
-          </p>
-          <button className="flex mt-[22px] justify-center py-[12px] px-[59px] text-white font-medium text-[16px] leading-[19px] rounded-lg bg-[#D21919] gap-[10px]">
-            Explore
-          </button>
-        </div>        
-      </div>
     </div>
   );
 };
 
-export default Video;
+export default Video2;
